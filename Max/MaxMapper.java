6f01b718ecd79755164f7de84fda302a65cd5d8e
Vanshi05 <vanshivanshivanshi@gmail.com>
2026-05-11 23:15:30 +0530

    steps

--- Max/MaxMapper.java
+++ Max/MaxMapper.java
@@ -16,3 +16,20 @@ public class MaxMapper extends Mapper<Object, Text, Text, IntWritable> {
         context.write(word, new IntWritable(number));
     }
 }
+
+jps
+start-dfs.sh
+start-yarn.sh
+
+javac -classpath `hadoop classpath` -d . MaxMapper.java MaxReducer.java MaxDriver.java
+jar -cvf maxnumber.jar *.class# Create an input directory in HDFS
+hdfs dfs -mkdir -p /user/input
+
+# Upload your local file to HDFS
+hdfs dfs -put input.txt /user/input/hadoop jar maxnumber.jar MaxDriver /user/input/input.txt /user/output/max_result
+
+# List the output files
+hdfs dfs -ls /user/output/max_result
+
+# View the content of the result
+hdfs dfs -cat /user/output/max_result/part-r-00000
--- Wordcount/WordCountMapper.java
+++ Wordcount/WordCountMapper.java
@@ -21,3 +21,20 @@ public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
         }
     }
 }
+
+jps
+start-dfs.sh
+start-yarn.sh
+
+javac -classpath $(hadoop classpath) -d . WordCountMapper.java WordCountReducer.java WordCountDriver.java
+jar -cvf wordcount.jar *.class
+
+# Create input directory (if not already there)
+hdfs dfs -mkdir -p /user/input
+
+# Upload your text file
+hdfs dfs -put sample_text.txt /user/input/
+
+hadoop jar wordcount.jar WordCountDriver /user/input/sample_text.txt /user/output/wordcount_results
+
+hdfs dfs -cat /user/output/wordcount_results/part-r-00000
